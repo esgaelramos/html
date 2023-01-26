0caf9e4dddf6082de3989676418afad7a013e7ab
Gael Ramos <esgaelramos@gmail.com>
2023-01-25 21:00:07 -0600

    Testing integration with php

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php require('./layout/header.php') ?>
+    <main>
+        <h1>Working with PHP</h1>
+        <?php require('./layout/faqs.php') ?>
+    </main>
+<?php require('./layout/footer.php') ?>
