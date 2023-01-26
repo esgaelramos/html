bad1fcd56f7ec7f1ad078382bfa4b599f0184e55
Gael Ramos <esgaelramos@gmail.com>
2023-01-26 15:56:09 -0600

    Testing add anothers PHP files

---
+++ rosa.php
@@ -0,0 +1,1 @@
+<?php require('./layout/header.php') ?><head><title>GIRA PREPA | COMIPEMS</title><meta name=description content><meta name=keywords content="gira prepa, comipems,"></head><main><h1>ROSA GIRA PREPA</h1><?php require('./layout/faqs.php') ?></main><?php require('./layout/footer.php') ?>
