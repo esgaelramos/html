22340b1db425c4663ec9942193b4ac01733f8446
Gael Ramos <esgaelramos@gmail.com>
2023-01-26 14:27:54 -0600

    Reorder repos and files again

--- index.php
+++ index.php
@@ -1,1 +1,1 @@
-<?php require('./layout/header.php') ?><head><title>GIRA PREPA | COMIPEMS</title><meta name=description content><meta name=keywords content="gira prepa, comipems,"></head><main><h1>Working with great PHP</h1><?php require('./layout/faqs.php') ?></main><?php require('./layout/footer.php') ?>
+<?php require('./layout/header.php') ?><head><title>GIRA PREPA | COMIPEMS</title><meta name=description content><meta name=keywords content="gira prepa, comipems,"></head><main><h1>Working in PHP</h1><?php require('./layout/faqs.php') ?></main><?php require('./layout/footer.php') ?>
