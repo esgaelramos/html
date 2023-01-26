6fc67aef9e1297012a1ef9d8b902bd3810f35284
Gael Ramos <esgaelramos@gmail.com>
2023-01-26 13:08:16 -0600

    Testing new script for min files, and re-order de SH again

--- test1.php
+++
@@ -1,1 +0,0 @@
-<?php require('./layout/header.php') ?><head><title>GIRA PREPA | COMIPEMS</title><meta name=description content><meta name=keywords content="gira prepa, comipems,"></head><main><h1>TEST 1</h1><?php require('./layout/faqs.php') ?></main><?php require('./layout/footer.php') ?>
