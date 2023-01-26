f205f5b9f7d833073f7481a950bd91cec08e214c
Gael Ramos <esgaelramos@gmail.com>
2023-01-25 21:10:29 -0600

    Testing integration with php again

--- layout/index.php
+++
@@ -1,6 +0,0 @@
-<?php require('./layout/header.php') ?>
-    <main>
-        <h1>Working with PHP</h1>
-        <?php require('./layout/faqs.php') ?>
-    </main>
-<?php require('./layout/footer.php') ?>
