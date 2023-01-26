c18fdcee330c67c0911c430e15d34fde94583567
Gael Ramos <esgaelramos@gmail.com>
2023-01-25 20:58:19 -0600

    Testing integration with php

---
+++ layout/header.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="logo icon" href="assets/logo.ico">
+    <title>GIRA PREPA | COMIPEMS</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="gira prepa, comipems,">
+    <meta name="author" content="GIRA PREPA" />
+    <meta name="copyright" content="Copyright GIRA 2023" />
+    <!-- ---------- STYLES ---------- -->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <!-- ---------- FONTS ---------- -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/2c36e9b7b1.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <!-- ----------
+        HEADER    
+    ---------- -->
+    <header id="header_navbar">
+        <nav class="navbar" >
+            <div class="navbar-logo">
+                <a href="/">
+                    <img src="assets/logo.svg" alt="GIRA LOGO">
+                </a>
+            </div>
+            <div class="navbar-items" id="_items">
+                <a href="/">CURSO</a>
+                <a href="/">PLANES</a>
+                <a href="/">BLOG</a>
+                <a href="/">GUIA</a>
+                <a href="/">CONTACTO</a>
+                <button class="switch" id="switch">
+					<span><i class="fas fa-sun"></i></span>
+					<span><i class="fas fa-moon"></i></span>
+				</button>
+            </div>
+            <div class="navbar-toggle" id="_toggle">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </nav>
+    </header>
+    <br><br>
