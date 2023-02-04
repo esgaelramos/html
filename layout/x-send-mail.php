ffa9c84a395bd306850e9339aadf24fa39482494
Gael Ramos <esgaelramos@gmail.com>
2023-02-03 20:28:28 -0600

    Add ip to contact form

--- layout/x-send-mail.php
+++ layout/x-send-mail.php
@@ -5,6 +5,7 @@ $nombre = $_POST['name'];
 $correo = $_POST['email'];
 $telefono = $_POST['phone'];
 $mensaje = $_POST['message'];
+$ip = $_SERVER['REMOTE_ADDR'];
 
 // Datos para el correo
 $destinatario = "giraprepa+contact@gmail.com";
@@ -13,7 +14,8 @@ $asunto = "Contacto desde nuestra web";
 $carta = "De: $nombre \n";
 $carta .= "Correo: $correo \n";
 $carta .= "Telefono: $telefono \n";
-$carta .= "Mensaje: $mensaje";
+$carta .= "Mensaje: $mensaje\n";
+$carta .= "IP: $ip";
 
 // Enviando Mensaje
 mail($destinatario, $asunto, $carta);
