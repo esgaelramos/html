bc8eb6228c7b171c36bba0047d43a0f454249011
Gael Ramos Laptop <esgaelramos@gmail.com>
2023-02-05 16:24:09 -0600

    Test images with drop shadowand dark theme

--- layout/why-buy.php
+++ layout/why-buy.php
@@ -1,8 +1,14 @@
 <section class=why><h2 class=why-title>Ventajas COMIPEMS</h2><div class=why-items><div class=why-exp><div class=why-img><img src=https://i.ibb.co/fxb9dvK/gira-circle.webp alt="MEJORA TU PERFIL CVPROPIO" width=10 height=10></div><div class=why-text><p><strong>MEJORA TU PERFIL</strong><br> Hazte notar con un CV <b>perfecto</b>, muestra tus <b>habilidades</b>, explota tus <b>conocimientos</b>, y disfruta del empleo que deseas. </p></div></div><div class=why-exp><style>
                     #ltest {
-                        -webkit-filter: drop-shadow(1px 1px 0 black)
-                        drop-shadow(-1px -1px 0 black);
-                        filter: drop-shadow(1px 1px 0 black) 
-                        drop-shadow(-1px -1px 0 black);
+                        -webkit-filter: drop-shadow(5px 5px 0 black)
+                        drop-shadow(-5px -5px 0 black);
+                        filter: drop-shadow(5px 5px 0 black) 
+                        drop-shadow(-5px -5px 0 black);
+                    }
+                    body.dark #ltest {
+                        -webkit-filter: drop-shadow(5px 5px 0 white)
+                        drop-shadow(-5px -5px 0 white);
+                        filter: drop-shadow(5px 5px 0 white) 
+                        drop-shadow(-5px -5px 0 white);
                     }
                 </style><div class=why-img><img id=ltest src=https://i.ibb.co/dBYN2MR/l.png alt="SUPERA AL ATS CVPROPIO" width=10 height=10></div><div class=why-text><p><strong>SUPERA AL ATS</strong><br> Enfrenta el <b>filtro de selección preparado</b>, enamora al robot de recursos humanos, y <b>destaca</b> en tu entrevista sin excusas. </p></div></div><div class=why-exp><div class=why-img><img src=https://i.ibb.co/5jKnqbg/lecciones-animadas.png alt="MULTIPLICA TUS OPORTUNIDADES CVPROPIO"></div><div class=why-text><p><strong>MULTIPLICA TUS OPORTUNIDADES</strong><br> Con una hoja de vida <b>eficiente</b>, optimiza cada postulación, y <b>triunfa en el mercado laboral</b>. </p></div></div><div class=why-exp><div class=why-img><img src=https://i.ibb.co/fxb9dvK/gira-circle.webp alt="PROPIO, UNICO Y PERSONALIZADO CVPROPIO"></div><div class=why-text><p><strong>PROPIO, ÚNICO Y PERSONALIZADO</strong><br><b>Adaptamos tu currículum</b> a las ofertas que quieras, refleja tu <b>experiencia</b> y <b>aptitudes</b> como el experto que eres. </p></div></div><div class=why-exp><div class=why-img><img src=https://i.ibb.co/fxb9dvK/gira-circle.webp alt="ATRACTIVO Y DE ALTO IMPACTO"></div><div class=why-text><p><strong>ATRACTIVO Y DE ALTO IMPACTO</strong><br> Despunta desde la primera impresión, enamora al reclutador, y <b>sé el profesional que todos quieran contratar</b>. </p></div></div></div></section>
